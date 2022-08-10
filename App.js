3e6c74d9ecd3894915be5528fd68c2a85fd1f3b5
joaquinrapetti <j.rapetti92@gmail.com>
2022-08-10 12:35:24 -0300

    focustime complete

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, SafeAreaView, Platform } from "react-native";
+
+import { colors } from "./src/utils/colors";
+import Focus from "./src/features/Focus";
+import FocusHistory from "./src/features/FocusHistory";
+import Timer from "./src/features/Timer";
 
 export default function App() {
+  const [currentSubject, setCurrentSubject] = useState(null);
+  const [history, setHistory] = useState(["Temperature control"]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {!currentSubject ? (
+        <>
+          <Focus addSubject={setCurrentSubject} />
+          <FocusHistory history={history} />
+        </>
+      ) : (
+        <Timer
+          focusSubject={currentSubject}
+          onTimerEnd={(subject) => {
+            setHistory([...history, subject]);
+          }}
+          clearSubject={() => {
+            setCurrentSubject(null);
+          }}
+        />
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // SafeAreaView only works for IOS
+    // Need to use as a view container with Platform android options
+    paddingTop: Platform.OS === "android" ? 50 : 0,
+    backgroundColor: colors.darkBlue,
+  },
+  text: {
+    color: colors.white,
   },
 });
